db9fb0dc6b08ab77faaf7f05ac95cf394c94a8d6
pau Novella <pau.novella@ific.uv.es>
2017-06-20 10:13:04 +0200

    updates

--- scripts/plot_egrass_noHV.py
+++ scripts/plot_egrass_noHV.py
@@ -6,7 +6,7 @@ import time
 hman = HistoManager(True)
 
 runs = [4213,4218,4219,4220,4221,4222,4223] 
-for run in runs: hman.load("../jobOptions/AutoTrigAna_%i.root"%run,"%i_"%run)
+for run in runs: hman.load("../data/AutoTrigAna_%i.root"%run,"%i_"%run)
 
 
 time0,rate = Double(),Double()
@@ -22,6 +22,7 @@ for run in runs:
 
 hman.graph("myrate",[-0.5]+times,[0]+rates,0,[0]+erates)
 hman.axis("myrate_graphAxis","Time (min)","e^{-} grass rate (ms^{-1})")
+hman.setTitle("myrate_graphAxis","Auto-trigger Runs")
 hman.style1d()
 hman.drawGraph("myrate","AP",markerType=20,min=30,max=40)
 
@@ -34,6 +35,7 @@ hman.graph("Axis",[time1,time2],[0,0],0,0)
 hman["Axis_graphAxis"].GetXaxis().SetTimeDisplay(1);
 hman["Axis_graphAxis"].GetXaxis().SetTimeFormat("%H:%M");
 hman.axis("Axis_graphAxis","Time","e^{-} grass rate (ms^{-1})")
+hman.setTitle("Axis_graphAxis","Auto-trigger Runs")
 hman.style1d()
 hman.draw("Axis_graphAxis",max=40,min=30)
 for run in runs: hman.drawGraph("%i_S1Rate"%run,"P",markerType=20)
